a61465056f957c1dd2701bace905b736ac32bd84
LZWHZXHY <LZWHZXHY@gmail.com>
2025-09-23 10:20:20 -0400

    Week 1 reading 1b

--- week1/reading/test.js
+++ week1/reading/test.js
@@ -5,6 +5,11 @@ let list = [-2, 3, 1, 24, -8, -9, 10];
 
 let list2 = [1, 2, 3, 4, 5, 6];
 
+let isEven = x => x % 2 == 0;
+
+let key = ["a", "b", "c"];
+let value = [1, 2, 3];
+
 
 function evenIdxPositiveSum(list){
     console.log("==============================================");
@@ -22,18 +27,35 @@ function evenIdxPositiveSum(list){
     console.log("==============================================");
 }
 
-function filterer(list, boolean){
+function filterer(list, booleanFunc){
     console.log("==============================================");
     console.log("Function filterer start:");
     console.log("Input list:" + list);
 
+    let newList = [];
+    for(let i = 0; i<list.length; i++){
+        if(booleanFunc(list[i])){
+            newList.push(list[i])
+        }
+    }
+    console.log("new list: " + newList);
+    console.log("Function filterer Ends");
+    console.log("==============================================");
+}
 
-
-
+function builder(key, value){
+    console.log("==============================================");
+    console.log("Function Builder start:");
+    let obj = {};
+    for(i = 0; i<key.length; i++){
+        obj[key[i]] = value[i];
+    }
+    console.log("object: ", obj);
     console.log("==============================================");
 }
 
 
-evenIdxPositiveSum(list);
 
-filterer(list2, true);
+evenIdxPositiveSum(list);
+filterer(list2, isEven);
+builder(key, value);
